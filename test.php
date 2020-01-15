d58b676d7f71edb9d850710f7f0b5706b45cce36
trantuanngoctn97 <trantuanngoctn97@gmail.com>
2020-01-15 09:24:24 +0700

    fill test.php file

--- test.php
+++ test.php
@@ -1,7 +1,11 @@
-1
-2
-3
-4
-5
-6
-7
+#### Tài khoản công việc, config mặc định. 
+Host p.zamba.vn
+HostName p.zamba.vn
+User ngoctt
+IdentityFile ~/.ssh/id_rsa_work
+
+#### Tài khoản cá nhân. git@github.com:trantuanngoctn97/AdminTemplateLaravel.git
+Host github.com
+Hostname github.com
+User trantuanngoctn97
+IdentityFile ~/.ssh/id_rsa_personal
