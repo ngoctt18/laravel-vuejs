a7153739416c088e094b0a39a9777c694c84f0f5
trantuanngoctn97 <trantuanngoctn97@gmail.com>
2020-01-15 14:53:05 +0700

    update config and dependencies

--- _config.php
+++ _config.php
@@ -1,10 +1,10 @@
-#### Tài khoản công việc, config mặc định. 
+#### Tài khoản công việc. git@p.zamba.vn:websolutions/vinfastthanglong.git
 Host p.zamba.vn
 HostName p.zamba.vn
 User ngoctt
 IdentityFile ~/.ssh/id_rsa_work
 
-#### Tài khoản cá nhân. git@github.com:trantuanngoctn97/AdminTemplateLaravel.git
+#### Tài khoản cá nhân.   git@github.com:trantuanngoctn97/AdminTemplateLaravel.git
 Host github.com
 Hostname github.com
 User trantuanngoctn97
