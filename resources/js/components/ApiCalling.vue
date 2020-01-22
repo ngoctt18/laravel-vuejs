f87d9d7f5ecc6e8bdbdb1d3d226c6adb1834a78a
trantuanngoctn97 <trantuanngoctn97@gmail.com>
2020-01-22 11:12:33 +0700

    success code :v

--- resources/js/components/ApiCalling.vue
+++ resources/js/components/ApiCalling.vue
@@ -135,23 +135,24 @@
         }
     }
 
-    // setInterval(function () {
-    //     $('.questionDropdownContainer span[data-index="1"]').click();
-    //     $('.surveyDashboardCTA').click();
-    // }, 7000);
-    //
-    // setInterval(function () {
-    //     let list = document.getElementsByClassName('section-place-with-edits-selected')[0].getElementsByClassName('section-verify-edit-not-sure');
-    //     for (let i = 0; i < list.length; i++) {
-    //         list[i].click();
-    //         console.log('click');
-    //     }
-    // }, 5000);
-    // setInterval(function () {
-    //     let scroll = document.getElementsByClassName('section-scrollbox')[0];
-    //     scroll.scrollTop = scroll.scrollHeight / 2;
-    //     console.log('scroll');
-    // }, 25000);
+    setInterval(function () {
+        $('.questionDropdownContainer span[data-index="1"]').click();
+        $('.surveyDashboardCTA').click();
+    }, 7000);
+
+
+    setInterval(function () {
+        let list = document.getElementsByClassName('section-place-with-edits-selected')[0].getElementsByClassName('section-verify-edit-not-sure');
+        for (let i = 0; i < list.length; i++) {
+            list[i].click();
+            console.log(list.length + ' click');
+        }
+    }, 5000);
+    setInterval(function () {
+        let scroll = document.getElementsByClassName('section-scrollbox')[0];
+        scroll.scrollTop = scroll.scrollHeight;
+        console.log('scroll');
+    }, 20000);
 
 
 </script>
